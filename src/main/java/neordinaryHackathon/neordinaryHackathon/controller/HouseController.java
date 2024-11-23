04e022ce53724f22a0d30b0d89f993f9bac14d6d
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2024-11-24 04:14:53 +0900

    operation 추가

--- src/main/java/neordinaryHackathon/neordinaryHackathon/controller/HouseController.java
+++ src/main/java/neordinaryHackathon/neordinaryHackathon/controller/HouseController.java
@@ -1,5 +1,6 @@
 package neordinaryHackathon.neordinaryHackathon.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -24,13 +25,14 @@ import java.util.List;
 public class HouseController {
     private final HouseService houseService;
 
+    @Operation(summary = "하우스 리스트 조회 API")
     @GetMapping("/houses")
     public BaseResponse<HouseResponseDto.getHousesResult> getHouses(@RequestParam("nickname") @Size(min = 1 ,max = 50, message = "소유자 이름은 255자를 초과할 수 없습니다.") String nickname) {
         List<House> houses = houseService.getHouses(nickname);
         List<HouseDto> houseDto = HouseConverter.toHouseDto(houses);
         return BaseResponse.onSuccess(HouseConverter.toGetHousesResult(houseDto));
     }
-
+    @Operation(summary = "하우스 삭제 API")
     @DeleteMapping("/houses/{houseId}")
     public BaseResponse<Void> deleteHouses(@PathVariable(name = "houseId") Long houseId) {
         houseService.deleteHouse(houseId);
@@ -42,13 +44,13 @@ public class HouseController {
         House house = houseService.createHouse(createHouse);
         return BaseResponse.onSuccess(HouseConverter.toCreateHouseResult(house));
     }
-
+    @Operation(summary = "하우스 단 건 조회 API")
     @GetMapping("/houses/{houseId}")
     public BaseResponse<HouseResponseDto.GetHouseResult> getHouse(@PathVariable(name = "houseId") Long houseId) {
         House house = houseService.getHouse(houseId);
         return BaseResponse.onSuccess(HouseConverter.toHouseDto(house));
     }
-
+    @Operation(summary = "하우스 수정 API")
     @PatchMapping("/houses")
     public BaseResponse<HouseResponseDto.UpdateHouseResult> updateHouse(@RequestBody @Valid HouseRequestDto.UpdateHouse request) {
         House house = houseService.updateHouse(request);
