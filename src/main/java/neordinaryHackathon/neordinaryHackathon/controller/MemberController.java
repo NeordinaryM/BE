e416d3937b3307cd451a53ff4a4a9cb5c1b6db00
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-11-24 06:52:18 +0900

    fajwelifj

--- src/main/java/neordinaryHackathon/neordinaryHackathon/controller/MemberController.java
+++ src/main/java/neordinaryHackathon/neordinaryHackathon/controller/MemberController.java
@@ -1,6 +1,7 @@
 package neordinaryHackathon.neordinaryHackathon.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import neordinaryHackathon.neordinaryHackathon.apiPayload.BaseResponse;
 import neordinaryHackathon.neordinaryHackathon.dto.MemberResponseDTO;
@@ -15,14 +16,14 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Operation(summary = "회원가입 API")
+    @Operation(summary = "회원가입 API", description = "쌍따옴표를 제외하고 테스트해주세요. (쌍따옴표도 DB에 들어갑니다.)")
     @PostMapping("/signup")
     public BaseResponse<MemberResponseDTO.signupDto> signUp(@RequestBody String nickname) {
         MemberResponseDTO.signupDto signupDTO = memberService.signup(nickname);
         return BaseResponse.onSuccess(signupDTO);
     }
 
-    @Operation(summary = "로그인 API")
+    @Operation(summary = "로그인 API", description = "쌍따옴표를 제외하고 테스트해주세요. (쌍따옴표도 DB에 들어갑니다.)")
     @PostMapping("/login")
     public BaseResponse<MemberResponseDTO.loginDto> login(@RequestBody String nickname) {
         MemberResponseDTO.loginDto loginDto = memberService.login(nickname);
